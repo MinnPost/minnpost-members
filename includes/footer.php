7331b3c9154be304aef88ed1da200ba9e9e9330f
Jonathan Stegall <jonathanstegall@MP-LT-23-MacAir.local>
2015-04-30 12:21:14 -0500

    convert it to sass and grunt.. too much minifying

--- includes/footer.php
+++ includes/footer.php
@@ -8,8 +8,8 @@
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="//maps.googleapis.com/maps/api/js?sensor=false&amp;libraries=places"></script>
-<script src="/js/jquery.geocomplete.min.js"></script>
-<script src="/js/members.min.js"></script>
+<script src="/js/vendor/jquery.geocomplete.min.js"></script>
+<script src="/js/main.min.js"></script>
 
 </body>
 </html>
---
+++ js/src/main.js
@@ -0,0 +1,69 @@
+/*jshint smarttabs:true */
+// main members js
+
+function differentAddressForShipping(checked) {
+	if (checked === true) {
+		$('.shipping_address').show();
+	} else {
+		$('.shipping_address').hide();
+	}
+}
+
+function existingSubscription(status) {
+if (status === 'existing') {
+	$('.atlantic_id').show();
+		$('.atlantic_id input').attr('required', 'required');
+	} else {
+		$('.atlantic_id').hide();
+	}
+}
+
+function getFullAddress() {
+
+	$('.not-geocomplete').addClass('offscreen');
+	$('.use-geocomplete').show();
+
+	/*$('#full_address').geocomplete({
+	details: 'form',
+	detailsAttribute: 'data-geo'
+	});
+	$('#full_address').click(function(){
+	$('#full_address').trigger('geocode');
+	});*/
+
+	$('.geocomplete').click(function(){
+		$(this).trigger('geocode');
+
+		var attribute = $(this).closest('fieldset').attr('data-geo');
+
+		$(this).geocomplete({
+			details: 'form',
+			detailsAttribute: attribute
+		});
+
+	});
+
+}
+
+$(function() {
+	/*if (google.hasOwnProperty('maps')) {
+		getFullAddress();
+	}*/
+	$('.use-geocomplete').hide();
+});
+
+$(document).ready(function() {
+
+	differentAddressForShipping(false);
+	$('#use_different_address').change(function() {
+		var c = this.checked;
+		differentAddressForShipping(c);
+	});
+
+	existingSubscription(false);
+	$('form input[name=atlantic_status]').on('change', function() {
+		var status = $('input[name=atlantic_status]:checked', 'form').val();
+		existingSubscription(status);
+	});
+
+});
