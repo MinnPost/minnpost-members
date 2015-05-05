a3821eab439af6acc0c088b516235b48da5fd989
Jonathan Stegall <jonathanstegall@MP-LT-23-MacAir.local>
2015-05-05 16:24:27 -0500

    update the success message

--- includes/partner-offers/message.php
+++ includes/partner-offers/message.php
@@ -1,7 +1,7 @@
 <section>
 	<h3 class="component-label">Thank you for supporting MinnPost</h3>
 <?php if ($success == TRUE) { ?>
-	<p>You have claimed your partner offer.</p>
+	<p>You have claimed your offer! You will receive an email from MinnPost membership coordinator Ashleigh Swenson later today with more details. Thank you for your support of MinnPost!</p>
 	<?php print_r($message); ?>
 <?php } else { ?>
 	<p>Sorry, this offer was already claimed. You might still be able to claim <a href="/partner-offers.php?id=<?php echo $salesforce_id; ?>">another offer</a>; otherwise try again next month!</p>
