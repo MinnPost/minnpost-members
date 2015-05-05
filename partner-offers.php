1c0b212804f892dc6b575665464b313957c42349
Jonathan Stegall <jonathanstegall@MP-LT-23-MacAir.local>
2015-05-05 17:15:05 -0500

    max width so the error message doesn't break stuff

--- partner-offers.php
+++ partner-offers.php
@@ -1,3 +1,8 @@
+<?php
+header("Cache-Control: no-store, must-revalidate, max-age=0");
+header("Pragma: no-cache");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+?>
 <?php $title = 'Claim Partner Offers'; ?>
 <?php include('includes/head.php'); ?>
 <?php include('includes/partner-offers/run.php'); ?>    
