05c15581b67f6377897c3a43aca0df31a0ffcec8
Jonathan Stegall <jstegall@JONATHANs-MacBook-Pro.local>
2015-06-01 10:22:23 -0500

    button

--- includes/partner-offers/ineligible.php
+++ includes/partner-offers/ineligible.php
@@ -2,5 +2,5 @@
 	<h3 class="component-label">Become a MinnPost member to claim offers</h3>
 	<p>Partner offers are made available to MinnPost Gold and Platinum members, who contribute the equivalent of $10 per month or more.</p>
 	<p>(If you believe you already are a MinnPost member and have received this message in error, please contact Ashleigh at <a href="mailto:aswenson@minnpost.com">aswenson@minnpost.com</a> or 612-455-6954.)</p>
-	<p><a href="http://www.minnpost.com/support/?amount=10" class="button next">Become a MinnPost member</a> <a class="benefits" href="http://www.minnpost.com/support/member-benefits?level=gold">Learn about other MinnPost member benefits</a></p>
+	<p><a href="http://www.minnpost.com/support/?amount=10" class="btn btn--final">Become a MinnPost member</a> <a class="benefits" href="http://www.minnpost.com/support/member-benefits?level=gold">Learn about other MinnPost member benefits</a></p>
 </section>
