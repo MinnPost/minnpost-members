2eaa8c261b71b3655107e333d1d3cb23df850223
Jonathan Stegall <jstegall@MP-LT-27-Macbook-2.local>
2015-07-13 10:28:09 -0500

    add a fake contact id for debugging

--- includes/partner-offers/eligibility.php
+++ includes/partner-offers/eligibility.php
@@ -5,6 +5,9 @@ $local_id = $account['id'];
 if ($_POST['contact_id']) {
 	$local_id = $_POST['contact_id'];
 }
+if (isset($_GET['debug']) && $_GET['debug'] === 'true') {
+	$local_id = 123456789;
+}
 if ($local_id) {
 	$exists = TRUE;
 	$user_claim_limit = "SELECT * FROM offer_instances WHERE contact_id='$local_id' AND claimed BETWEEN DATE_SUB(NOW(), INTERVAL $eligibility_period_mysql) AND NOW() ORDER BY claimed DESC";
