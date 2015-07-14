86af4b3ae1da411e354186557355ae8307278c1c
Jonathan Stegall <jstegall@MP-LT-27-Macbook-2.local>
2015-07-14 12:33:28 -0500

    rename atlantic stuff to thank-you-gifts

--- atlantic-subscriptions.php
+++
@@ -1,4 +0,0 @@
-<?php $title = 'Atlantic Subscription Address'; ?>
-<?php include('includes/head.php'); ?>
-<?php include('includes/atlantic/run.php'); ?>    
-<?php include('includes/footer.php'); ?>
---
+++ thank-you-gifts.php
@@ -0,0 +1,4 @@
+<?php $title = 'Redeem Thank You Gifts'; ?>
+<?php include('includes/head.php'); ?>
+<?php include('includes/thank-you-gifts/run.php'); ?>    
+<?php include('includes/footer.php'); ?>
