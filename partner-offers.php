80bbd5198b01548b96b51cbd1cab50f40aa5d4dc
Jonathan Stegall <jstegall@minnpost.com>
2019-01-16 15:18:05 -0600

    remove partner-offers.php from this repo

--- partner-offers.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Cache-Control: no-store, must-revalidate, max-age=0");
-header("Pragma: no-cache");
-header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
-?>
-<?php $title = 'Claim Partner Offers'; ?>
-<?php include('includes/head.php'); ?>
-<?php include('includes/partner-offers/run.php'); ?>    
-<?php include('includes/footer.php'); ?>
